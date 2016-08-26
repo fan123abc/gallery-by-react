fa12c6162cbeb034345e185d7685b51e860c802c
joe <fan123abc@qq.com>
2016-08-26 14:42:23 +0800

    init project structor

---
+++ src/components/GalleryByReactApp.js
@@ -0,0 +1,33 @@
+/*'use strict';*/
+
+var React = require('react/addons');
+
+// CSS
+require('normalize.css');
+require('../styles/main.scss');
+//引入图片信息的json格式
+var imageDatas = require('../data/imagDatas.json');
+function genImageURL(imageDatasArr){
+	for(var i = 0, j = imageDatasArr.length; i < j; i++){
+		var singleImageData = imageDatasArr[i];
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+		imageDatasArr[i] = singleImageData;
+	}
+	return imageDatasArr;
+}
+imageDatas = genImageURL(imageDatas);
+var GalleryByReactApp = React.createClass({
+  render: function() {
+    return (
+      <section className="stage">
+          <section className="img-sec">
+          </section>
+          <nav className="controller-nav">
+          </nav>
+      </section>
+    );
+  }
+});
+React.render(<GalleryByReactApp />, document.getElementById('content')); // jshint ignore:line
+
+module.exports = GalleryByReactApp;
